a908056726b221d096b5701d65735bc4327d0185
soumyas <so.singh1226@gmail.com>
2021-01-10 01:10:00 -0500

    initial set up of all files and connection

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name) VALUES ('Fish Fillet Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Chicken Burger');
+INSERT INTO burgers (burger_name) VALUES ('Double Cheese Beef Burger');
